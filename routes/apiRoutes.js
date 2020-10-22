f75b16b959ac35047826c2916d679fc49013841c
Ava Lorusso <ava.tlorusso@gmail.com>
2020-10-22 19:41:58 -0400

    updated api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,28 @@
-const router = require("express").Router();
+const router = require("express").app();
 const store = require("../db/store");
-const { get } = require("./htmlRoutes");
 
-router.get("/api/notes", function (req, res) {
+router.get("/notes", function (req, res) {
   store
+  res.json(notes);
+});
 
-  // this code will execute when the server receives a GET request to api/notes
-  getNotes();
+fs.writeFile('/db/store', data, (err) => {
+  if (err) throw err;
+  console.log('Note taken');
 
-  // read what is in db.json => using fs module
-  fs.readFile("../db.store", function (err, data) {
-
-    // Check for errors 
+  app.post("/notes", function (req, res) {
+    store
+      .addNote(req.body)
+    res.json(notes);
     if (err) throw err;
   })
 
-  // send response with data
-  router.post("/api/notes", function (req, res) {
-    const newNote = req.body;
+  app.get("/api/notes", function (req, res) {
+    res.json(data);
+    res.json(true)
+  })
 
-    // take new note and add it to db.json using fs module
-    app.get("/api/notes", function (req, res) {
-      res.json(data);
-    })
-
-    // send response that the note was saved
-    app.post("/api/notes", function (req, res) {
-      res.json(data);
-    })
-  });
-
-  // DELETE "/api/notes" deletes the note with an id equal to req.params.id
-  router.delete("/notes/:id", (req, res) => {
+  app.delete("/notes/:id", (req, res) => {
     store
       .removeNote(req.params.id)
       .then(() => res.json({ ok: true }))
