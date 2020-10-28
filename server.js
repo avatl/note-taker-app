3646fe546d88ca562353684d01a0270106e7dac8
Ava Lorusso <ava.tlorusso@gmail.com>
2020-10-28 14:51:00 -0400

    make it better

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require("express")
 const apiRoutes = require("routes/apiRoutes.js");
 const htmlRoutes = require("routes/htmlRoutes.js");
-
+//Server 
 const app = express();
-
-const PORT = 3000;
-
-app.use((req, res, next) => {
-    console.log(req.url);
-
+// Port
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log("listening on port:",PORT)
+})
+// middleware
+app.use((req ,res,next)=>{
+    console.log (req.url);
     next();
 });
-
+// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("routes/apiRoutes.js", apiRoutes);
-app.use("routes/htmlRoutes.js", htmlRoutes);
-
-app.get('/', (req, res) => {
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+// route
+app.get('/',(req , res ) =>{
     res.send('got you!')
 })
-
-app.listen(PORT, () => {
+// Listen
+app.listen(PORT,()=>{
     console.log(`Server started on http:localhost:${PORT}`);
 }) 
