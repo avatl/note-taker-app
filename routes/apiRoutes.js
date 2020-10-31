6bbf1d9d14806cbf3912a81cc70eef0a20993268
Ava Lorusso <ava.tlorusso@gmail.com>
2020-10-31 13:41:15 -0400

    make it better

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,42 @@
 const store = require("../server");
-
+const router = require("express").Router();
+const fs = require("fs");
+const data = require("../db/db.json")
+// this code will execute when the server receives a GET request to api/notes
 router.get("/notes", function (req, res) {
   store
   res.json(notes);
 });
-
-fs.writeFile('/db/store', data, (err) => {
-  if (err) throw err;
-  console.log('Note taken');
-
-  app.post("/notes", function (req, res) {
-    store
-      .addNote(req.body)
-    res.json(notes);
-    if (err) throw err;
-  })
-
-  app.get("/api/notes", function (req, res) {
-    res.json(data);
-    res.json(true)
-  })
-  
-  app.delete("/notes/:id", (req, res) => {
-    store
-      .removeNote(req.params.id)
-      .then(() => res.json({ ok: true }))
-      .catch((err) => res.status(500).json(err));
+// read what is in db.json => using fs module
+() => {
+  return store.read().then((notes) => {
+    let parsedNotes;
+    // If notes isn't an array or can't be turned into one, send back a new empty array
+    try {
+      parsedNotes = [].concat(JSON.parse(notes));
+    } catch (err) {
+      parsedNotes = [];
+    }
+    return parsedNotes;
   });
+}
+// send response with data
+router.post("/notes", function (req, res) {
+  store
+    .addNote(req.body)
+  res.json(notes);
+  if (err) throw err;
+});
+// take new note and add it to db.json using fs module
+router.get("/api/notes", function (req, res) {
+  res.json(data);
+  res.json(true)
 })
-
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", (req, res) => {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require("express")
-const apiRoutes = require("routes/apiRoutes.js");
-const htmlRoutes = require("routes/htmlRoutes.js");
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
 //Server 
 const app = express();
+
 // Port
 var PORT = process.env.PORT || 3000;
-app.listen(PORT, function() {
-    console.log("listening on port:",PORT)
-})
-// middleware
-app.use((req ,res,next)=>{
-    console.log (req.url);
-    next();
-});
+
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
 // route
-app.get('/',(req , res ) =>{
+app.get('/', (req, res) => {
     res.send('got you!')
 })
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Listen
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server started on http:localhost:${PORT}`);
 }) 
