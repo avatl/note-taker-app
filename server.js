c203c99653b99758cc6b35c49907c1bc79d607e4
Ava Lorusso <ava.tlorusso@gmail.com>
2021-01-11 18:58:41 -0500

    index &server

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-const express = require("express")
+// Dependencies
+var express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
-//Server 
-const app = express();
-
-// Port
+// Sets up the Express App
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Set up body parsing, static, and route middleware
+// Sets up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+// Routes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-// route
-app.get('/', (req, res) => {
-    res.send('got you!')
-})
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+// Starts listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
-
-// Listen
-app.listen(PORT, () => {
-    console.log(`Server started on http:localhost:${PORT}`);
-}) 
